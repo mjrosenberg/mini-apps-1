4477435d9b9ec4e54f54ee055ffeaf6383758340
mjrosenberg <mjr70@duke.edu>
2020-06-08 15:47:22 -0700

    added styles.css

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,24 +1,23 @@
 const squares = document.getElementById('tictactoe');
 const button = document.getElementById('reset');
 const submitButton = document.getElementById('submit');
-var XMove = true;
-var squaresFilled = 0;
-var gameOver = false;
-var winner = '';
-var count = {X: 0, O: 0};
-var playerX = 'X';
-var playerO = 'O';
-//if (winner === 'X')
+var gameState = {};
+gameState.XMove = true;
+gameState.squaresFilled = 0;
+gameState.gameOver = false;
+gameState.winner = '';
+gameState.count = {X: 0, O: 0};
+gameState.playerX = 'X';
+gameState.playerO = 'O';
 var modifyText = (event) =>{
-  //console.dir(event.target.innerText);
-  if (event.target.innerText === '' && gameOver === false){
-    if (XMove === true) {
+  if (event.target.innerText === '' && gameState.gameOver === false){
+    if (gameState.XMove === true) {
       event.target.innerText = 'X';
     } else{
       event.target.innerText = 'O';
     }
-    XMove = !XMove;
-    squaresFilled++;
+    gameState.XMove = !gameState.XMove;
+    gameState.squaresFilled++;
   }
 }
 var checkRow = (row, letter) =>{
@@ -58,43 +57,41 @@ var checkPosDiagonal = (letter) =>{
   return true;
 }
 var checkBoard = (event)=>{
-  //after each click we should check the board to see if a win has occurred or it is full
-  if (gameOver === false){
-    //console.log(playerX, playerO);
+  if (gameState.gameOver === false){
     var id = event.target.id;
     var letter = event.target.innerText;
     var row = id[0];
     var col = id[1];
     var player;
     if (letter === "X"){
-      player = playerX;
+      player = gameState.playerX;
     }
     if (letter === "O"){
-      player = playerO;
+      player = gameState.playerO;
     }
     if (checkRow(row, letter) === true){
       //display winning message and return out of the function
       document.getElementById('ending').innerText = player + ' wins!';
-      gameOver= true;
-      winner = letter;
-      count[letter]++;
+      gameState.gameOver= true;
+      gameState.winner = letter;
+      gameState.count[letter]++;
       return;
     }
     if (checkCol(col, letter) === true){
       //display winning message and return out of the function
       document.getElementById('ending').innerText = player + ' wins!';
-      gameOver= true;
-      winner = letter;
-      count[letter]++;
+      gameState.gameOver= true;
+      gameState.winner = letter;
+      gameState.count[letter]++;
       return;
     }
     if (id === '11'|| id === '22'|| id === '33'){
       if (checkNegDiagonal(letter) === true){
         //display winning message and return out of the function
         document.getElementById('ending').innerText = player + ' wins!';
-        gameOver= true;
-        winner = letter;
-        count[letter]++;
+        gameState.gameOver= true;
+        gameState.winner = letter;
+        gameState.count[letter]++;
         return;
       }
     }
@@ -102,19 +99,18 @@ var checkBoard = (event)=>{
       if (checkPosDiagonal(letter) === true){
         //display winning message and return out of the function
         document.getElementById('ending').innerText = player + ' wins!';
-        gameOver= true;
-        winner = player;
-        count[letter]++;
+        gameState.gameOver= true;
+        gameState.winner = player;
+        gameState.count[letter]++;
         return;
       }
     }
-    if (squaresFilled === 9){
+    if (gameState.squaresFilled === 9){
       //display the tie message and return out of the function
       document.getElementById('ending').innerText = 'It\'s a tie! The board is full';
-      gameOver= true;
+      gameState.gameOver= true;
       return;
     }
-    //document.getElementById('ending').innerText = player+'\'s turn';
     if (letter === "X"){
       document.getElementById('ending').innerText = playerO + '\'s turn';
     }
@@ -124,42 +120,40 @@ var checkBoard = (event)=>{
   }
 }
 var reset = (event) => {
-  //squares.innerText = '';
   for (var row = 1; row<4; row++){
     for (var col =1 ; col<4; col++){
       var id = String(row) + String(col);
       document.getElementById(id).innerText = '';
     }
   }
-  //add a tally for x and o
-  if (winner === 'X'){
-    count.x++
-    document.getElementById('xtally').innerText = playerX + ': ' + count.X;
-    XMove = true;
-    document.getElementById('ending').innerText = playerX + '\'s turn';
-    document.getElementById('winner').innerText =  'Reigning Champion: ' + playerX;
-  } else if (winner === 'O'){
-    count.o++
-    document.getElementById('otally').innerText =  playerO + ': ' + count.O;
-    XMove = false;
-    document.getElementById('ending').innerText = playerO + '\'s turn';
-    document.getElementById('winner').innerText =  'Reigning Champion: ' + playerO;
+  if (gameState.winner === 'X'){
+    gameState.count.x++
+    document.getElementById('xtally').innerText = gameState.playerX + ': ' + gameState.count.X;
+    gameState.XMove = true;
+    document.getElementById('ending').innerText = gameState.playerX + '\'s turn';
+    document.getElementById('winner').innerText =  'Reigning Champion: ' + gameState.playerX;
+  } else if (gameState.winner === 'O'){
+    gameState.count.o++
+    document.getElementById('otally').innerText =  gameState.playerO + ': ' + gameState.count.O;
+    gameState.XMove = false;
+    document.getElementById('ending').innerText = gameState.playerO + '\'s turn';
+    document.getElementById('winner').innerText =  'Reigning Champion: ' + gameState.playerO;
   } else {
-    document.getElementById('xtally').innerText = playerX + ': ' + count.X;
-    document.getElementById('otally').innerText =  playerO + ': ' + count.O;
-    document.getElementById('ending').innerText = playerX + '\'s turn';
+    document.getElementById('xtally').innerText = gameState.playerX + ': ' + gameState.count.X;
+    document.getElementById('otally').innerText =  gameState.playerO + ': ' + gameState.count.O;
+    document.getElementById('ending').innerText = gameState.playerX + '\'s turn';
   }
-  squaresFilled = 0;
-  gameOver = false;
+  gameState.squaresFilled = 0;
+  gameState.gameOver = false;
 }
 var doSubmit = (event) =>{
   var xNameInput = document.getElementById('xname').value;
   var oNameInput = document.getElementById('oname').value;
   if(xNameInput !== null){
-    playerX = xNameInput;
+    gameState.playerX = xNameInput;
   }
   if(oNameInput !== null){
-    playerO = oNameInput;
+    gameState.playerO = oNameInput;
   }
   reset(event);
 }
