7e950e8d082a9614617bc98ebbc72543284d52a8
mjrosenberg <mjr70@duke.edu>
2020-06-09 10:18:48 -0700

    package set up and server started

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('client'));
+
+app.get('/', (req,res)=>{
+  //console.log('hello');
+  res.send('hello');
+});
+
+app.listen(port,()=>console.log('listening on port', port));
 
