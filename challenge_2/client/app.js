2245e8edfe4d86e20b2374a93fb292f7d1d70667
mjrosenberg <mjr70@duke.edu>
2020-06-09 10:35:49 -0700

    implemented post request on server without csv functionality

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,2 @@
-
+//build out the post request here and handle the response by displaying it on the index.html file
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,10 +3,21 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('client'));
+app.use(express.urlencoded());
 
 app.get('/', (req,res)=>{
   //console.log('hello');
   res.send('hello');
+  //idk if this is even needed
+});
+
+app.post('/submit',(req, res)=>{
+  var csv = '';
+  var jsonObj = req.body.jsonObj;
+  //req.body.jsonObj contains the values submitted to the form
+  //need to return a csv version of that
+  res.status(200).send(csv);
+
 });
 
 app.listen(port,()=>console.log('listening on port', port));
