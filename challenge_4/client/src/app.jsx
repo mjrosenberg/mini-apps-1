32ac450748793156416f0621ebb69bd0bc15a92e
mjrosenberg <mjr70@duke.edu>
2020-06-11 10:54:48 -0700

    separated into index.jsx for rendering, but still not working

--- challenge_4/client/src/app.jsx
+++ challenge_4/client/src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import Board from './components/Board.jsx';
 
 
@@ -35,10 +35,5 @@ class App extends React.Component {
   }
 }
 //display this onto the DOM
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
 export default App;
